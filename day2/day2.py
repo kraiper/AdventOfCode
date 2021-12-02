7b6287942b19908d766ae77ec15682abca0d77f8
Kraiper <martin_svart@hotmail.com>
2021-12-02 21:39:10 +0100

    Day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,34 @@
 file = open("input.txt")
 file_content = file.readlines()
 
-print(file_content)
+#p1
+depth = 0
+pos = 0
+for value in file_content:
+    command, value = value.split(" ")
+    value = int(value)
+    if command == "forward":
+        pos += value
+    elif command == "down":
+        depth += value
+    elif command == "up":
+        depth -= value
+
+print(depth*pos)
+
+#p2
+depth = 0
+pos = 0
+aim = 0
+for value in file_content:
+    command, value = value.split(" ")
+    value = int(value)
+    if command == "forward":
+        pos += value
+        depth += aim * value
+    elif command == "down":
+        aim += value
+    elif command == "up":
+        aim -= value
+
+print(depth*pos)
