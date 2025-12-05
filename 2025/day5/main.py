4dd2e8920b50f057823b984bfa0610329964c8ff
Martin <martin_svart@hotmail.com>
2025-12-05 19:55:19 +0100

    day 5

--- 2025/day5/main.py
+++ 2025/day5/main.py
@@ -20,39 +20,59 @@ def solution_a(data: str):
                     break
     return res
 
+def find_lowest_index(ranges):
+    lowest = -1
+    lowest_index = -1
+    for index, r in enumerate(ranges):
+        start, stop = r
+        if index == 0:
+            lowest = start
+            lowest_index = index
+        if lowest > start:
+            lowest = start
+            lowest_index = index
+    return lowest_index
+
+def find_overlaps(ranges, start, stop):
+    index = 0
+    while index < len(ranges):
+        r_start, r_stop = ranges[index]
+        if r_start <= stop:
+            if r_stop > stop:
+                stop = r_stop
+            ranges.pop(index)
+            index = 0
+        else:
+            index += 1
+    return (start, stop)
+
+
+def run_loop(ranges):
+    handled_intervalls = []
+    while ranges:
+        lowest_index = find_lowest_index(ranges)
+        start, stop = ranges.pop(lowest_index)
+        handled_intervalls.append(find_overlaps(ranges, start, stop))
+
+    return handled_intervalls
+
 def solution_b(data: str):
-    fresh_ranges = []
+    res = 0
+    raw_ranges = []
     for line in data.splitlines():
         if "-" in line:
             start, stop = line.split("-")
             start = int(start)
             stop = int(stop)
-            add = True
-            pops = []
-            for ran in fresh_ranges:
-                o_start, o_stop = ran
-                ### new range within old range
-                if start >= o_start and stop <= o_stop:
-                    add = False
-                    break
+            raw_ranges.append((start, stop))
 
-                ### 
-                if start >= o_start and start <= o_stop:
-                    start = o_stop + 1
-                if stop >= o_start and stop <= o_stop:
-                    stop = o_start - 1
+    handled_intervalls = []
+    ranges = raw_ranges.copy()
+    handled_intervalls = run_loop(ranges)
 
-            if add:
-                fresh_ranges.append((start, stop))
-
-    res = 0
-    for x in fresh_ranges:
-        start, stop = x
-        # for v in range(start, stop + 1):
-        #     print(v)
-
+    for intervall in handled_intervalls:
+        start, stop = intervall
         res += stop - start + 1
-
     return res
 
 puzzle = Puzzle(year=2025, day=5)
@@ -68,9 +88,9 @@ print(f"Solution_a: {sol_answer_a} - time taken: {end - start}")
 answer_b = "14"
 assert answer_b is not None
 sol_b = str(solution_b(example.input_data))
-assert int(sol_b) > 330256053047770
 assert sol_b == str(answer_b), f"Got {sol_b}, expected {answer_b}"
 start = time.time()
 sol_answer_b = str(solution_b(puzzle.input_data))
+assert int(sol_answer_b) > 332023203631941, f"{sol_answer_b}"
 end = time.time()
 print(f"Solution_b: {sol_answer_b} - time taken: {end - start}")
